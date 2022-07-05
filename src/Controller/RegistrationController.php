23966882e15d5ed82653f80ec64931e71316175a
alla <allaabdelazize@gmail.com>
2022-07-05 14:54:43 +0200

    mail setting

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -53,7 +53,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('contact@gtrouver.com', 'gtrouve'))
+                    ->from(new Address('contact@alla-abdelaziz.com', 'gtrouve'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
